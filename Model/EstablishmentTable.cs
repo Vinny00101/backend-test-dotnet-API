8e915a4c0f342a0c18e0ed00699ebc8ee9dfbbc1
Vinny00101 <vini123gtbr@gmail.com>
2025-05-04 17:44:10 -0300

    feat: Implement table for Establishment with DataAnnotations

---
+++ Model/EstablishmentTable.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend_test_dotnet_API.Model;
+
+public class EstablishmentTable
+{
+    public Guid id {get; private set;}
+
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [MaxLength(100)]
+    public string? Nome {get; private set;}
+
+    [Required(ErrorMessage = "O CNPJ é obrigatório.")]
+    [MaxLength(14, ErrorMessage = "CNPJ deve conter exatamente 14 dígitos.")]
+    [RegularExpression(@"^\d{14}$", ErrorMessage = "CNPJ deve conter apenas números.")]
+    public string? Cnpj {get; private set;} 
+
+    [Required(ErrorMessage = "Endereço é obrigatório.")]
+    [MaxLength(255, ErrorMessage = "Endereço deve conter somente 255 dígitos.")]
+    public string? Endereco {get; private set;}
+
+    [Required(ErrorMessage = "O Telefone é obrigatório.")]
+    [MaxLength(20, ErrorMessage = "Telefone deve conter exatamente 20 dígitos.")]
+    public string? Telefone {get; private set;}
+
+    [Required(ErrorMessage = "Quantidade de vagas para motos é obrigatório.")]
+    [Range(0, 1000, ErrorMessage = "O valor deve estar entre 0 e 1000.")]
+    public int vagasMotos {get; private set;}
+
+    [Required(ErrorMessage = "Quantidade de vagas para carros é obrigatório.")]
+    [Range(0, 1000, ErrorMessage = "O valor deve estar entre 0 e 1000.")]
+    public int vagasCarros {get; private set;}
+}
