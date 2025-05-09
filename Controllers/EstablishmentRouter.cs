1ece88dc3597f0b140566795853946be15c5a219
Vinny00101 <vini123gtbr@gmail.com>
2025-05-09 19:57:03 -0300

    feat: controller to CRUD Establishment

--- Controllers/EstablishmentRouter.cs
+++ Controllers/EstablishmentRouter.cs
@@ -1,4 +1,8 @@
 using System;
+using backend_test_dotnet_API.Data;
+using backend_test_dotnet_API.Dtos;
+using backend_test_dotnet_API.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend_test_dotnet_API.Controllers;
 
@@ -8,20 +12,34 @@ public static class EstablishmentRouter
     {
         var router = app.MapGroup("estabelecimento");
 
-        router.MapPost("adicionar", () => {
+        router.MapPost("post", async (EstablishmentDTO request, AppDbContext _context) => { 
+            var errors = ValidatorDTO.ValidationModel(request);
+            if(errors.Any())
+            {
+                return Results.BadRequest(errors);
+            }
 
+            var Establishment = new EstablishmentTable(
+                request.Nome,
+                request.Cnpj,
+                request.Endereco,
+                request.Telefone,
+                request.vagasMotos,
+                request.vagasCarros
+            );
+            await _context.Estabelecimento.AddAsync(Establishment);
+            await _context.SaveChangesAsync();
+
+            return Results.Ok();
         });
 
-        router.MapGet("buscar", () => {
-
+        router.MapGet("buscar", async (AppDbContext _context) => {
+            var result = await _context.Estabelecimento
+                .Select( e => new {
+                    e.id
+                }).ToListAsync();
+            return Results.Ok(result);
         });
-
-        router.MapPut("editar", () => {
-
-        });
-
-        router.MapDelete("remove", () => {
-
-        });
+        // rota que buscar os veiculos no estabelecimento/id do estabelecimento
     }
 }
